e2dd22393ac5f4678b23d813b02946e47d62d9a2
Martin Eisenbarth <eyesee@foobarlab.net>
2020-07-14 11:41:37 +0200

    add Docker support

--- scripts/85-docker.sh
+++ scripts/85-docker.sh
@@ -7,12 +7,19 @@ fi
 
 # Docker and tooling
 
-#sudo emerge -vt app-emulation/docker
-#sudo /usr/share/docker/contrib/check-config.sh
+sudo emerge -vt app-emulation/docker
+sudo /usr/share/docker/contrib/check-config.sh
 
-##sudo emerge -vt app-emulation/docker-swarm
-#sudo emerge -vt dev-util/docker-ls
-#sudo emerge -vt app-emulation/docker-compose
+cat <<'DATA' | sudo tee -a /etc/docker/daemon.json
+{
+    "debug": true
+}
+DATA
 
-##sudo rc-update add docker default
-#sudo usermod -aG docker vagrant
+sudo usermod -aG docker vagrant
+
+#sudo rc-update add docker default
+
+#sudo emerge -vt app-emulation/docker-swarm
+sudo emerge -vt dev-util/docker-ls
+sudo emerge -vt app-emulation/docker-compose
