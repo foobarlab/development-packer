5e41e637de89635480a8181b690141cc1a9dd09a
Martin Eisenbarth <eyesee@foobarlab.net>
2020-07-14 11:41:54 +0200

    more tools

--- scripts/80-tools.sh
+++ scripts/80-tools.sh
@@ -5,8 +5,11 @@ if [ -z ${BUILD_RUN:-} ]; then
   exit 1
 fi
 
+# apache
+sudo emerge -vt app-admin/apachetop
+
 # some network related utils
-sudo emerge -vt net-dns/bind-tools net-analyzer/iptraf-ng net-analyzer/nmap net-analyzer/openbsd-netcat net-analyzer/iftop
+sudo emerge -vt net-misc/iperf sys-apps/ethtool net-dns/bind-tools net-analyzer/iptraf-ng net-analyzer/nmap net-analyzer/openbsd-netcat net-analyzer/iftop
 
 # housekeeping utils
 #sudo emerge -vt sys-apps/bleachbit
