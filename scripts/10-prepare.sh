9dbcf5ebc837ffd0746eedd9e7d10d22ff427db3
Martin Eisenbarth <eyesee@foobarlab.net>
2020-07-14 14:40:51 +0200

    update motd / issue

--- scripts/10-prepare.sh
+++ scripts/10-prepare.sh
@@ -11,21 +11,23 @@ sed -i 's/<br>/\n/g' /home/vagrant/.$BUILD_BOX_NAME
 sudo rm -f /etc/motd
 cat <<'DATA' | sudo tee -a /etc/motd
 
-Funtoo GNU/Linux Vagrant Box (BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
+Funtoo GNU/Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME) - release BUILD_BOX_VERSION build BUILD_TIMESTAMP
 
 DATA
 sudo sed -i 's/BUILD_BOX_NAME/'"$BUILD_BOX_NAME"'/g' /etc/motd
+sudo sed -i 's/BUILD_BOX_USERNAME/'"$BUILD_BOX_USERNAME"'/g' /etc/motd
 sudo sed -i 's/BUILD_BOX_VERSION/'"$BUILD_BOX_VERSION"'/g' /etc/motd
 sudo sed -i 's/BUILD_TIMESTAMP/'"$BUILD_TIMESTAMP"'/g' /etc/motd
 sudo cat /etc/motd
 
 sudo rm -f /etc/issue
 cat <<'DATA' | sudo tee -a /etc/issue
-This is a Funtoo GNU/Linux Vagrant Box (BUILD_BOX_NAME BUILD_BOX_VERSION)
+This is a Funtoo GNU/Linux Vagrant Box (BUILD_BOX_USERNAME/BUILD_BOX_NAME BUILD_BOX_VERSION)
 
 DATA
 sudo sed -i 's/BUILD_BOX_VERSION/'$BUILD_BOX_VERSION'/g' /etc/issue
 sudo sed -i 's/BUILD_BOX_NAME/'$BUILD_BOX_NAME'/g' /etc/issue
+sudo sed -i 's/BUILD_BOX_USERNAME/'"$BUILD_BOX_USERNAME"'/g' /etc/issue
 sudo cat /etc/issue
 
 sudo mkdir -p /etc/portage/package.use
