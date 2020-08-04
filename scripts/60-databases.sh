4f60a197064f1ff7b007f905797cc08b052927df
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-04 20:24:32 +0200

    basic configuration for mariadb/mysql and postgresql

--- scripts/60-databases.sh
+++ scripts/60-databases.sh
@@ -11,23 +11,30 @@ sudo emerge -vt dev-db/mariadb
 #sudo rc-update add mysql default
 
 # initially configure mariadb (create initial databases, set root passwd)
-#cat <<'DATA' | sudo tee -a /root/.my.cnf
-#[client]
-#host     = localhost
-#user     = root
-#password = BUILD_MYSQL_ROOT_PASSWORD
-#DATA
-#sudo sed -i 's/BUILD_MYSQL_ROOT_PASSWORD/'"$BUILD_MYSQL_ROOT_PASSWORD"'/g' /root/.my.cnf
-#sudo emerge --config dev-db/mariadb
-#sudo rm -f /root/.my.cnf
+cat <<'DATA' | sudo tee -a /root/.my.cnf
+[client]
+host     = localhost
+user     = root
+password = BUILD_MYSQL_ROOT_PASSWORD
+DATA
+sudo sed -i 's/BUILD_MYSQL_ROOT_PASSWORD/'"$BUILD_MYSQL_ROOT_PASSWORD"'/g' /root/.my.cnf
+sudo emerge --config dev-db/mariadb || true
+sudo rm -f /root/.my.cnf
 
 # FIXME adjust root password & secure install in Ansible later
-# TODO secure install, see: https://www.funtoo.org/Package:MariaDB
+
+# TODO questionaire and secures the mariadb installation, see: https://www.funtoo.org/Package:MariaDB
 #sudo mysql_secure_installation
+
+# for ansible mysql integration we need python-mysql
+#sudo emerge -vt dev-python/mysql-python
 
 # ---- PostgreSQL
 
-sudo emerge -vt dev-db/postgresql
+sudo emerge -vt dev-db/postgresql app-eselect/eselect-postgresql
+
+# basic configuration
+sudo emerge --config dev-db/postgresql || true
 
 # ---- Sqlite
 
