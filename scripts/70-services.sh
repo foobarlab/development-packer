79f4800a9c7972df86030304e123250f8282b50b
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-04 20:27:01 +0200

    webserver configs, add caddy webserver

--- scripts/70-services.sh
+++ scripts/70-services.sh
@@ -9,6 +9,9 @@ fi
 
 sudo emerge -vt www-servers/apache www-apache/mod_security www-apache/modsecurity-crs app-admin/apachetop
 
+# add some more (optional) modules
+sudo emerge -vt www-apache/mod_bw www-apache/mod_common_redirect www-apache/mod_dnsbl_lookup www-apache/mod_evasive www-apache/mod_fastcgi_handler www-apache/mod_fcgid www-apache/mod_geoip2 www-apache/mod_limitipconn www-apache/mod_log_sql www-apache/mod_qos www-apache/mod_tidy www-apache/mod_umask www-apache/mod_xsendfile
+
 # set global server name to avoid annoying warning message on startup
 cat <<'DATA' | sudo tee -a /etc/apache2/httpd.conf
 
@@ -17,6 +20,33 @@ ServerName BUILD_BOX_NAME
 
 DATA
 sudo sed -i 's/BUILD_BOX_NAME/'"$BUILD_BOX_NAME"'/g' /etc/apache2/httpd.conf
+
+# set some DEFINE flags to enable stuff:
+#  AUTH_DIGEST  Enables mod_auth_digest
+#  AUTHNZ_LDAP  Enables authentication through mod_ldap (available if USE=ldap)
+#  CACHE        Enables mod_cache
+#  DAV          Enables mod_dav
+#  ERRORDOCS    Enables default error documents for many languages.
+#  INFO         Enables mod_info, a useful module for debugging
+#  LANGUAGE     Enables content-negotiation based on language and charset.
+#  LDAP         Enables mod_ldap (available if USE=ldap)
+#  MANUAL       Enables /manual/ to be the apache manual (available if USE=docs)
+#  MEM_CACHE    Enables default configuration mod_mem_cache
+#  PROXY        Enables mod_proxy
+#  SSL          Enables SSL (available if USE=ssl)
+#  STATUS       Enabled mod_status, a useful module for statistics
+#  SUEXEC       Enables running CGI scripts (in USERDIR) through suexec.
+#  USERDIR      Enables /~username mapping to /home/username/public_html
+sudo grep -e '-D LANGUAGE' /etc/conf.d/apache2 > /dev/null || sudo sed -ir 's/APACHE2_OPTS="\(.*\)"/APACHE2_OPTS="\1 -D LANGUAGE"/g' /etc/conf.d/apache2
+
+# configure apache to use PHP: enable '-D PHP' in APACHE2_OPTS if not already enabled ...
+sudo grep -e '-D PHP' /etc/conf.d/apache2 > /dev/null || sudo sed -ir 's/APACHE2_OPTS="\(.*\)"/APACHE2_OPTS="\1 -D PHP"/g' /etc/conf.d/apache2
+
+# configure apache to use PHP: enable '-D SECURITY' in APACHE2_OPTS if not already enabled ...
+sudo grep -e '-D SECURITY' /etc/conf.d/apache2 > /dev/null || sudo sed -ir 's/APACHE2_OPTS="\(.*\)"/APACHE2_OPTS="\1 -D SECURITY"/g' /etc/conf.d/apache2
+
+# hint: https://wiki.gentoo.org/wiki/Project:Apache/Troubleshooting
+# hint: https://wiki.gentoo.org/wiki/Project:Apache/Upgrading
 
 # configure apache to use PHP: enable '-D PHP' in APACHE2_OPTS if not already enabled ...
 #sudo grep -e '-D PHP' /etc/conf.d/apache2 > /dev/null || sudo sed -ir 's/APACHE2_OPTS="\(.*\)"/APACHE2_OPTS="\1 -D PHP"/g' /etc/conf.d/apache2
@@ -34,6 +64,14 @@ sudo emerge -vt www-servers/nginx app-admin/ngxtop
 # ---- Lighttpd
 
 sudo emerge -vt www-servers/lighttpd
+
+# ---- Caddy
+
+sudo emerge -vt www-servers/caddy
+
+# ---- Let's encrypt
+
+sudo emerge -vt app-crypt/certbot app-crypt/certbot-apache app-crypt/certbot-nginx
 
 # ---- Varnish proxy cache
 
@@ -53,4 +91,4 @@ sudo emerge -vt mail-mta/postfix
 
 # ---- Avahi / mDNS
 
-sudo emerge -vt net-dns/avahi sys-auth/nss-mdns
+sudo emerge -vt net-dns/avahi sys-auth/nss-mdns dev-python/zeroconf
