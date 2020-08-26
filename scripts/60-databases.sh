c876d9ddb3f22738662752ac405f4a2321463d47
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-26 11:44:13 +0200

    minor mariadb changes

--- scripts/60-databases.sh
+++ scripts/60-databases.sh
@@ -7,7 +7,7 @@ fi
 
 # ---- MariaDB (MySQL)
 
-sudo emerge -vt dev-db/mariadb dev-db/mysqltuner
+sudo emerge -vt dev-db/mariadb dev-db/mariadb-connector-c dev-db/mysqltuner
 #sudo rc-update add mysql default
 
 # initially configure mariadb (create initial databases, set root passwd)
@@ -18,13 +18,8 @@ user     = root
 password = BUILD_MYSQL_ROOT_PASSWORD
 DATA
 sudo sed -i 's/BUILD_MYSQL_ROOT_PASSWORD/'"$BUILD_MYSQL_ROOT_PASSWORD"'/g' /root/.my.cnf
-sudo emerge --config dev-db/mariadb || true
+sudo emerge --config dev-db/mariadb || true    # FIXME this actually fails, but /var/lib/mysql is populated ...
 sudo rm -f /root/.my.cnf
-
-# FIXME adjust root password & secure install in Ansible later
-
-# TODO questionaire and secures the mariadb installation, see: https://www.funtoo.org/Package:MariaDB
-#sudo mysql_secure_installation
 
 # various mysql integrations
 sudo emerge -vt dev-python/pymysql dev-python/mysqlclient
