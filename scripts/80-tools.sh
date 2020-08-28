6369dade7531ebe135a06dacb9a5a4781923a7b2
Martin Eisenbarth <eyesee@foobarlab.net>
2020-08-28 22:04:10 +0200

    add various tools (vcs, vim plugins, jq)

--- scripts/80-tools.sh
+++ scripts/80-tools.sh
@@ -11,6 +11,58 @@ fi
 #sudo emerge -vt sys-apps/etckeeper
 #sudo etckeeper init -d /etc
 
+# ---- various version control systems (other than Git)
+
+sudo emerge -vt \
+  dev-vcs/subversion \
+  dev-vcs/mercurial \
+  dev-vcs/cvs
+
+# ---- various vim plugins
+
+sudo emerge -vt \
+  app-vim/bash-support \
+  app-vim/vimpython \
+  app-vim/vim-go \
+  app-vim/rust-vim \
+  app-vim/csv \
+  app-vim/align \
+  app-vim/ansiesc \
+  app-vim/ant_menu \
+  app-vim/closetag \
+  app-vim/dhcpd-syntax \
+  app-vim/editorconfig-vim \
+  app-vim/eselect-syntax \
+  app-vim/gentoo-syntax \
+  app-vim/gist \
+  app-vim/gitlog \
+  app-vim/greputils \
+  app-vim/json \
+  app-vim/notes \
+  app-vim/rails \
+  app-vim/scala-syntax \
+  app-vim/screen \
+  app-vim/securemodelines \
+  app-vim/surround \
+  app-vim/tagbar \
+  app-vim/taglist \
+  app-vim/tasklist \
+  app-vim/udev-syntax \
+  app-vim/vcscommand \
+  app-vim/vim-commentary \
+  app-vim/vim-multiple-cursors \
+  app-vim/vim-spell-de \
+  app-vim/vim-spell-en \
+  app-vim/vim-spell-fr \
+  app-vim/vim-spell-pt \
+  app-vim/vimcommander \
+  app-vim/vimoutliner \
+  app-vim/wikipedia-syntax \
+  app-vim/xsl-syntax \
+  app-vim/webapi
+
+# TODO add: app-vim/vim2hs, app-vim/vimclojure,  
+
 # ---- various network utils
 
 sudo emerge -vt \
@@ -23,7 +75,6 @@ sudo emerge -vt \
   net-analyzer/nmap \
   net-analyzer/openbsd-netcat \
   net-analyzer/iftop \
-  net-dns/dnswalk \
   net-dns/dnstop \
   net-analyzer/dnstracer \
   net-analyzer/dhcpdump \
@@ -47,7 +98,8 @@ sudo emerge -vt \
   sys-apps/ripgrep \
   app-arch/pigz \
   app-shells/fzf \
-  app-text/multitail
+  app-text/multitail \
+  app-misc/jq
 
 # ---- gfx/video utils
 
