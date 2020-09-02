6087c54e89698eff5b0186245f3262b554356d94
Martin Eisenbarth <eyesee@foobarlab.net>
2020-09-02 11:25:43 +0200

    move to embedded

--- scripts/50-languages.sh
+++ scripts/50-languages.sh
@@ -143,12 +143,3 @@ sudo emerge -vt dev-lang/rust-bin
 # ---- Whitespace
 
 sudo emerge -vt dev-lang/whitespace
-
-# ---- ARM / AVR / Arduino
-
-sudo emerge -vt dev-embedded/avrdude dev-embedded/avra
-sudo emerge -vt dev-embedded/arduino-builder sys-devel/crossdev
-
-# ---- ESP8266/ ESP32
-
-emerge -vt dev-embedded/esptool
