efa3ed8174e35b4432a196132fba3dc1156d95dc
Martin Eisenbarth <eyesee@foobarlab.net>
2020-11-19 07:27:19 +0100

    switch overlay branch

--- config.sh
+++ config.sh
@@ -24,7 +24,7 @@ export BUILD_BOX_MEMORY="2048"
 export BUILD_CUSTOM_OVERLAY=true
 export BUILD_CUSTOM_OVERLAY_NAME="foobarlab"
 export BUILD_CUSTOM_OVERLAY_URL="https://github.com/foobarlab/foobarlab-overlay.git"
-export BUILD_CUSTOM_OVERLAY_BRANCH="master"
+export BUILD_CUSTOM_OVERLAY_BRANCH="development"   # set to 'master' for more stable builds
 
 # TODO make finalize step optional, like:
 #export BUILD_AUTO_FINALIZE=false  # if 'true' automatically run finalize.sh script
