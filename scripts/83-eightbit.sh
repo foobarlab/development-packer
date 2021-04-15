cea13e65493e58d979f38d1815a3ed25f35d9bfd
Martin Eisenbarth <eyesee@foobarlab.net>
2021-04-15 11:39:58 +0200

    update notes

--- scripts/83-eightbit.sh
+++ scripts/83-eightbit.sh
@@ -7,7 +7,8 @@ fi
 
 # ---- 6502 8-bit CPU programming (Commodore, Apple)
 
+# TODO initiate new kit like 'retro-kit'?
 # TODO ebuilds for Assemblers: 64TASS, Acme, CC65, KickAss
 # TODO ebuilds for Crunchers: pucrunch, exomizer
 # TODO move VICE emulator from user-tools here
-# TODO ebuilds for other C64 tools: sprite, char, gfx, diredit, ...
+# TODO ebuilds for other C64 tools: sprite, char, gfx, diredit, music composing (goattracker)...
